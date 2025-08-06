cab0d3eae618de7b85a9f9f6bf684f2699773748
Ahmedalmannaei <ahmed.almannaie18@gmail.com>
2025-08-06 18:20:53 +0300

    added try catch to routes and fixed names

--- controllers/event.js
+++ controllers/event.js
@@ -3,8 +3,27 @@ const mongoose = require("mongoose");
 const Event = require("../models/events");
 
 router.post("/", async (req, res) => {
-  const event = await Event.create(req.body);
-  res.status(201).json(event);
+  try {
+    const event = await Event.create(req.body);
+    res.status(201).json(event);
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
 });
-
+router.get("/:id", async (req, res) => {
+  try {
+    const events = await Event.find({ owner: req.sessionID }); //.populate("owner") will add later
+    res.status(201).json(events);
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+});
+router.get("/all", async (req, res) => {
+  try {
+    const events = await Event.find(); //.populate("owner") will add later
+    res.status(201).json(events);
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+});
 module.exports = router;
