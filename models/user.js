f9d39535ca632594a58d186ddf9f955f383d8640
muneraisa <muneeraisa33@gmail.com>
2025-08-06 09:35:34 +0300

    updated userModel

--- models/user.js
+++ models/user.js
@@ -1,14 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
+    required: true
   },
-  password: {
+  hashedPassword: {
     type: String,
-    required: true,
-  },
+    required: true
+  }
 });
 
-module.exports = mongoose.model("User", userSchema);
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    delete returnedObject.hashedPassword;
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
