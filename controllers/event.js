a26bab75f91d2348cbd4e0de193f747a521d8f2e
Ahmedalmannaei <ahmed.almannaie18@gmail.com>
2025-08-08 21:15:14 +0300

    added delete and update routes and also fixed post and get

--- controllers/event.js
+++ controllers/event.js
@@ -1,18 +1,24 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const Event = require("../models/events");
+const verifyToken = require("../middleware/verify-token");
 
-router.post("/", async (req, res) => {
+router.post("/", verifyToken, async (req, res) => {
   try {
-    const event = await Event.create(req.body);
+    const ownerId = req.user._id;
+    const event = await Event.create({
+      ...req.body,
+      owner: ownerId,
+    });
     res.status(201).json(event);
   } catch (err) {
     res.status(400).json({ err: err.message });
   }
 });
-router.get("/:id", async (req, res) => {
+router.get("/show/:id", async (req, res) => {
   try {
-    const events = await Event.find({ owner: req.sessionID }); //.populate("owner") will add later
+    const userId = req.params.id;
+    const events = await Event.find({ owner: userId }).populate("owner");
     res.status(201).json(events);
   } catch (err) {
     res.status(400).json({ err: err.message });
@@ -20,10 +26,29 @@ router.get("/:id", async (req, res) => {
 });
 router.get("/all", async (req, res) => {
   try {
-    const events = await Event.find(); //.populate("owner") will add later
+    const events = await Event.find().populate("owner");
     res.status(201).json(events);
   } catch (err) {
     res.status(400).json({ err: err.message });
   }
+});
+// router.get("/details/:id", async (req, res) => {});
+router.put("/:id", verifyToken, async (req, res) => {
+  const userId = req.user._id;
+  const event = await Event.findById(req.params.id);
+  if (!event.owner.equals(userId)) {
+    return res.status(403).send("Only owner can delete");
+  }
+  const updatedEvent = await Event.findByIdAndUpdate(req.params.id, req.body);
+  res.status(200).json(updatedEvent);
+});
+router.delete("/:id", verifyToken, async (req, res) => {
+  const userId = req.user._id;
+  const event = await Event.findById(req.params.id);
+  if (!event.owner.equals(userId)) {
+    return res.status(403).send("Only owner can delete");
+  }
+  const deleteEvent = await Event.findByIdAndDelete(req.params.id);
+  res.status(200).json(deleteEvent);
 });
 module.exports = router;
